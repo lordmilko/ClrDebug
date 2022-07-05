fa9dd7ac83b80252467afd79f9da6d7b69543fa7
lordmilko <lordmilko@users.noreply.github.com>
2022-07-05 19:32:21 +1000

    Add ToString override on methods containing a Name property for better debugger display

--- ClrDebug/Managed/XCLRData/XCLRDataAppDomain.cs
+++ ClrDebug/Managed/XCLRData/XCLRDataAppDomain.cs
@@ -197,5 +197,14 @@ namespace ClrDebug
 
         #endregion
         #endregion
+        
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
