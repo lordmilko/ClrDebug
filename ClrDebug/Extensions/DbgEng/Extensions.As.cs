8353a47fcbd7be00f5d7b15d30467f9740178eae
lordmilko <lordmilko@users.noreply.github.com>
2023-01-18 11:04:51 +1100

    Implement As<T>() extension methods for casting wrappers to different wrapper types their underlying object supports

---
+++ ClrDebug/Extensions/DbgEng/Extensions.As.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace ClrDebug.DbgEng
+{
+    public static partial class DbgEngExtensions
+    {
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the dbgeng!DebugClient type.<para/>
+        /// Possible conversions include <see cref="DebugClient"/>, <see cref="DebugAdvanced"/>,
+        /// <see cref="DebugControl"/>, <see cref="DebugRegisters"/>, <see cref="DebugSymbols"/>
+        /// and <see cref="DebugSystemObjects"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces DebugClient supports.</typeparam>
+        /// <param name="debugClient">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this DebugClient debugClient) =>
+            AsDebugClient<T, IDebugClient>(debugClient.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the dbgeng!DebugClient type.<para/>
+        /// Possible conversions include <see cref="DebugClient"/>, <see cref="DebugAdvanced"/>,
+        /// <see cref="DebugControl"/>, <see cref="DebugRegisters"/>, <see cref="DebugSymbols"/>
+        /// and <see cref="DebugSystemObjects"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces DebugClient supports.</typeparam>
+        /// <param name="debugAdvanced">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this DebugAdvanced debugAdvanced) =>
+            AsDebugClient<T, IDebugAdvanced>(debugAdvanced.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the dbgeng!DebugClient type.<para/>
+        /// Possible conversions include <see cref="DebugClient"/>, <see cref="DebugAdvanced"/>,
+        /// <see cref="DebugControl"/>, <see cref="DebugRegisters"/>, <see cref="DebugSymbols"/>
+        /// and <see cref="DebugSystemObjects"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces DebugClient supports.</typeparam>
+        /// <param name="debugControl">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this DebugControl debugControl) =>
+            AsDebugClient<T, IDebugControl>(debugControl.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the dbgeng!DebugClient type.<para/>
+        /// Possible conversions include <see cref="DebugClient"/>, <see cref="DebugAdvanced"/>,
+        /// <see cref="DebugControl"/>, <see cref="DebugRegisters"/>, <see cref="DebugSymbols"/>
+        /// and <see cref="DebugSystemObjects"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces DebugClient supports.</typeparam>
+        /// <param name="debugRegisters">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this DebugRegisters debugRegisters) =>
+            AsDebugClient<T, IDebugRegisters>(debugRegisters.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the dbgeng!DebugClient type.<para/>
+        /// Possible conversions include <see cref="DebugClient"/>, <see cref="DebugAdvanced"/>,
+        /// <see cref="DebugControl"/>, <see cref="DebugRegisters"/>, <see cref="DebugSymbols"/>
+        /// and <see cref="DebugSystemObjects"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces DebugClient supports.</typeparam>
+        /// <param name="debugSymbols">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this DebugSymbols debugSymbols) =>
+            AsDebugClient<T, IDebugSymbols>(debugSymbols.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the dbgeng!DebugClient type.<para/>
+        /// Possible conversions include <see cref="DebugClient"/>, <see cref="DebugAdvanced"/>,
+        /// <see cref="DebugControl"/>, <see cref="DebugRegisters"/>, <see cref="DebugSymbols"/>
+        /// and <see cref="DebugSystemObjects"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces DebugClient supports.</typeparam>
+        /// <param name="debugSystemObjects">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this DebugSystemObjects debugSystemObjects) =>
+            AsDebugClient<T, IDebugSystemObjects>(debugSystemObjects.Raw);
+
+        private static TResult AsDebugClient<TResult, TRaw>(IntPtr raw)
+        {
+            var t = typeof(TResult);
+            object result;
+
+            if (t == typeof(DebugClient))
+                result = new DebugClient(raw);
+            else if (t == typeof(DebugAdvanced))
+                result = new DebugAdvanced(raw);
+            else if (t == typeof(DebugControl))
+                result = new DebugControl(raw);
+            else if (t == typeof(DebugRegisters))
+                result = new DebugRegisters(raw);
+            else if (t == typeof(DebugSymbols))
+                result = new DebugSymbols(raw);
+            else if (t == typeof(DebugSystemObjects))
+                result = new DebugSystemObjects(raw);
+            else
+                throw Extensions.GetAsNotSupportedException<TResult, TRaw>();
+
+            return (TResult) result;
+        }
+    }
+}
---
+++ ClrDebug/Extensions/Extensions.As.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+namespace ClrDebug
+{
+    public static partial class Extensions
+    {
+        #region ClrDataAccess
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the mscordacwks!ClrDataAccess type.<para/>
+        /// Possible conversions include <see cref="SOSDacInterface"/>, <see cref="XCLRDataProcess"/>
+        /// and <see cref="CLRDataEnumMemoryRegions"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces ClrDataAccess supports.</typeparam>
+        /// <param name="sosDacInterface">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this SOSDacInterface sosDacInterface) =>
+            AsClrDataAccess<T, ISOSDacInterface>(sosDacInterface.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the mscordacwks!ClrDataAccess type.<para/>
+        /// Possible conversions include <see cref="SOSDacInterface"/>, <see cref="XCLRDataProcess"/>
+        /// and <see cref="CLRDataEnumMemoryRegions"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces ClrDataAccess supports.</typeparam>
+        /// <param name="xclrDataProcess">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this XCLRDataProcess xclrDataProcess) =>
+            AsClrDataAccess<T, IXCLRDataProcess>(xclrDataProcess.Raw);
+
+        private static TResult AsClrDataAccess<TResult, TRaw>(object raw)
+        {
+            var t = typeof(TResult);
+            object result;
+
+            if (t == typeof(SOSDacInterface))
+                result = new SOSDacInterface((ISOSDacInterface) raw);
+            else if (t == typeof(XCLRDataProcess))
+                result = new XCLRDataProcess((IXCLRDataProcess) raw);
+            else if (t == typeof(CLRDataEnumMemoryRegions))
+                result = new CLRDataEnumMemoryRegions((ICLRDataEnumMemoryRegions) raw);
+            else
+                throw GetAsNotSupportedException<TResult, TRaw>();
+
+            return (TResult) result;
+        }
+
+        #endregion
+        #region ClrDataModule
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the mscordacwks!ClrDataModule type.<para/>
+        /// Possible conversions include <see cref="MetaDataImport"/> and <see cref="XCLRDataModule"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces ClrDataModule supports.</typeparam>
+        /// <param name="xclrDataModule">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this XCLRDataModule xclrDataModule)
+        {
+            var t = typeof(T);
+            object result;
+
+            if (t == typeof(MetaDataImport))
+                result = new MetaDataImport((IMetaDataImport) xclrDataModule.Raw);
+            else if (t == typeof(XCLRDataModule))
+                result = xclrDataModule;
+            else
+                throw GetAsNotSupportedException<T, IXCLRDataModule>();
+
+            return (T) result;
+        }
+
+        #endregion
+        #region RegMeta
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the clr!RegMeta type.<para/>
+        /// Possible conversions include <see cref="MetaDataImport"/>, <see cref="MetaDataAssemblyImport"/>,
+        /// <see cref="MetaDataEmit"/>, <see cref="MetaDataAssemblyEmit"/>, <see cref="MetaDataTables"/>
+        /// and <see cref="MetaDataInfo"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces RegMeta supports.</typeparam>
+        /// <param name="metaDataImport">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this MetaDataImport metaDataImport) =>
+            AsRegMeta<T, IMetaDataImport>(metaDataImport.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the clr!RegMeta type.<para/>
+        /// Possible conversions include <see cref="MetaDataImport"/>, <see cref="MetaDataAssemblyImport"/>,
+        /// <see cref="MetaDataEmit"/>, <see cref="MetaDataAssemblyEmit"/>, <see cref="MetaDataTables"/>
+        /// and <see cref="MetaDataInfo"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces RegMeta supports.</typeparam>
+        /// <param name="metaDataAssemblyImport">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this MetaDataAssemblyImport metaDataAssemblyImport) =>
+            AsRegMeta<T, IMetaDataAssemblyImport>(metaDataAssemblyImport.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the clr!RegMeta type.<para/>
+        /// Possible conversions include <see cref="MetaDataImport"/>, <see cref="MetaDataAssemblyImport"/>,
+        /// <see cref="MetaDataEmit"/>, <see cref="MetaDataAssemblyEmit"/>, <see cref="MetaDataTables"/>
+        /// and <see cref="MetaDataInfo"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces RegMeta supports.</typeparam>
+        /// <param name="metaDataEmit">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this MetaDataEmit metaDataEmit) =>
+            AsRegMeta<T, IMetaDataEmit>(metaDataEmit.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the clr!RegMeta type.<para/>
+        /// Possible conversions include <see cref="MetaDataImport"/>, <see cref="MetaDataAssemblyImport"/>,
+        /// <see cref="MetaDataEmit"/>, <see cref="MetaDataAssemblyEmit"/>, <see cref="MetaDataTables"/>
+        /// and <see cref="MetaDataInfo"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces RegMeta supports.</typeparam>
+        /// <param name="metaDataAssemblyEmit">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this MetaDataAssemblyEmit metaDataAssemblyEmit) =>
+            AsRegMeta<T, IMetaDataAssemblyEmit>(metaDataAssemblyEmit.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the clr!RegMeta type.<para/>
+        /// Possible conversions include <see cref="MetaDataImport"/>, <see cref="MetaDataAssemblyImport"/>,
+        /// <see cref="MetaDataEmit"/>, <see cref="MetaDataAssemblyEmit"/>, <see cref="MetaDataTables"/>
+        /// and <see cref="MetaDataInfo"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces RegMeta supports.</typeparam>
+        /// <param name="metaDataTables">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this MetaDataTables metaDataTables) =>
+            AsRegMeta<T, IMetaDataTables>(metaDataTables.Raw);
+
+        /// <summary>
+        /// Creates a <see cref="ComObject{T}"/> around an interface supported by the clr!RegMeta type.<para/>
+        /// Possible conversions include <see cref="MetaDataImport"/>, <see cref="MetaDataAssemblyImport"/>,
+        /// <see cref="MetaDataEmit"/>, <see cref="MetaDataAssemblyEmit"/>, <see cref="MetaDataTables"/>
+        /// and <see cref="MetaDataInfo"/>.
+        /// </summary>
+        /// <typeparam name="T">A type that wraps one of the interfaces RegMeta supports.</typeparam>
+        /// <param name="metaDataInfo">The existing wrapper to create the new wrapper from.</param>
+        /// <returns>A wrapper of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="NotSupportedException">A type is specified that is not known to this function.</exception>
+        public static T As<T>(this MetaDataInfo metaDataInfo) =>
+            AsRegMeta<T, IMetaDataInfo>(metaDataInfo.Raw);
+
+        private static TResult AsRegMeta<TResult, TRaw>(object raw)
+        {
+            var t = typeof(TResult);
+            object result;
+
+            if (t == typeof(MetaDataImport))
+                result = new MetaDataImport((IMetaDataImport) raw);
+            else if (t == typeof(MetaDataAssemblyImport))
+                result = new MetaDataAssemblyImport((IMetaDataAssemblyImport) raw);
+
+            else if (t == typeof(MetaDataEmit))
+                result = new MetaDataEmit((IMetaDataEmit) raw);
+            else if (t == typeof(MetaDataAssemblyEmit))
+                result = new MetaDataAssemblyEmit((IMetaDataAssemblyEmit) raw);
+
+            else if (t == typeof(MetaDataTables))
+                result = new MetaDataTables((IMetaDataTables) raw);
+            else if (t == typeof(MetaDataInfo))
+                result = new MetaDataInfo((IMetaDataInfo) raw);
+            else
+                throw GetAsNotSupportedException<TResult, TRaw>();
+
+            return (TResult) result;
+        }
+
+        #endregion
+
+        internal static NotSupportedException GetAsNotSupportedException<TResult, TRaw>()
+        {
+            return new NotSupportedException($"Casting '{typeof(TRaw).Name}' to a wrapper of type '{typeof(TResult).Name}' is not supported. If {typeof(TRaw).Name} supports the wrapper's underlying interface, please construct the wrapper manually.");
+        }
+    }
+}
