15a210d3c2d57c85b47ae6e3d6b80a15c1b09da8
lordmilko <lordmilko@users.noreply.github.com>
2023-03-25 12:44:41 +1100

    Add ICLRRuntimeLocator

---
+++ ClrDebug/Managed/CLRRuntimeLocator.cs
@@ -0,0 +1,37 @@
+﻿namespace ClrDebug
+{
+    public class CLRRuntimeLocator : ComObject<ICLRRuntimeLocator>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CLRRuntimeLocator"/> class.
+        /// </summary>
+        /// <param name="raw">The raw COM interface that should be contained in this object.</param>
+        public CLRRuntimeLocator(ICLRRuntimeLocator raw) : base(raw)
+        {
+        }
+
+        #region ICLRRuntimeLocator
+        #region RuntimeBase
+
+        public CLRDATA_ADDRESS RuntimeBase
+        {
+            get
+            {
+                CLRDATA_ADDRESS baseAddress;
+                TryGetRuntimeBase(out baseAddress).ThrowOnNotOK();
+
+                return baseAddress;
+            }
+        }
+
+        public HRESULT TryGetRuntimeBase(out CLRDATA_ADDRESS baseAddress)
+        {
+            /*HRESULT GetRuntimeBase(
+            [Out] out CLRDATA_ADDRESS baseAddress);*/
+            return Raw.GetRuntimeBase(out baseAddress);
+        }
+
+        #endregion
+        #endregion
+    }
+}
---
+++ ClrDebug/Native/Interfaces/ICLRRuntimeLocator.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace ClrDebug
+{
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("B760BF44-9377-4597-8BE7-58083BDC5146")]
+    [ComImport]
+    public interface ICLRRuntimeLocator
+    {
+        [PreserveSig]
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        HRESULT GetRuntimeBase(
+            [Out] out CLRDATA_ADDRESS baseAddress);
+    }
+}
